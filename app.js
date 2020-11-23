8a591b72e5efb948672e2457ec6869d98c0e173a
Georgina <georginakinslow@yahoo.com>
2020-11-22 20:06:28 -0600

    Completed Spread Destructuring Exercise.

---
+++ app.js
@@ -0,0 +1,79 @@
+// 1a. 
+const mcuShows = ["The Falcon And The Winter Soldier", "WandaVision"];
+
+//1b.
+const starWarsShows = ["The Mandalorian", "The Clone Wars"];
+
+//1c. 
+const disneyPlusShows = [...mcuShows, ...starWarsShows, "The Right Stuff"];
+
+//1d.
+console.log(disneyPlusShows);
+
+//2a.
+const netflixMovies = {action: `Extraction`, drama: `The Irishman`};
+
+//2b. 
+const disneyPlusMovies = {musical: `Hamilton`, drama: `Togo`};
+
+//2c.
+const streamingMovies = {comedy: `The Big Sick`, musical: `Hamilton`, action: `Extraction`, drama: `Togo`, drama:`The Irishman`};
+
+//2d.
+console.log(streamingMovies);
+
+//3a. 
+const disneyJunior = [`PJ Masks`, `Jake and the Neverland Pirates`];
+
+//3b.
+const pj = [`PJ Masks`];
+const jake = [`Jake and the Neverland Pirates`];
+
+//3c. 
+console.log(pj, jake);
+
+//4a. 
+const avengers = [captainAmerica: Steve Rogers, theHulk: Bruce Banner];
+
+//4b. 
+const captainAmerica = [`Steve Rogers`];
+const theHulk = [`Bruce Banner`];
+
+//4c. 
+console.log(captainAmerica, theHulk);
+
+//4d.
+const moreAvengers = [blackWidow: `Natasha Romanoff`, hawkeye: `Clint Barton`, ironMan: `Tony Stark`];
+
+//4e.
+const nat = [`Natasha Rogers`]; 
+const capt = [`Steve Rogers`];
+const hulk = [`Bruce Banner`];
+const rest = [`Others`];
+
+//4f. 
+console.log(nat, rest);
+
+//BONUS
+const bonus = {
+    first: [1, 2, 3],
+    second: [4, 5, 6],
+    third: [7, 8, 9]
+  };
+
+  //5a.
+  const all = [`first`, `second`, `third`];
+
+  //5b.
+  const bonus = {`... all};
+  const one = {1};
+  const two = {2};
+  const three = {3};
+  const four = {4};
+  const five = {5};
+  const six = {6}'
+  const seven = {7};
+  const eight = {8};
+  const nine = {9};
+  console.log(all);
+
